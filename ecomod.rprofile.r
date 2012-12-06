0dbe958ee59b7b3a134d6f58df921d1884240b61
jae0 <choijaeseok@gmail.com>
2012-12-06 10:46:34 -0400

    Update ecomod.rprofile.r

--- ecomod.rprofile.r
+++ ecomod.rprofile.r
@@ -1,32 +1,31 @@
-
-	# ecomod.directory
-	if (is.null( ecomod.directory )) {
-		stop( "Please define 'ecomod.directory' in your Rprofile.site (Windows) or .Rprofile (Linux/MacOS" ) 
-	}
-
-
-	# ecomod.workdirectory
-	if (is.null( ecomod.workdirectory )) {
-		ecomod.workdirectory = getwd()
-	} else {
-		setwd( ecomod.workdirectory )
-	}
+# ecomod.directory
+if (is.null( ecomod.directory )) {
+	stop( "Please define 'ecomod.directory' in your Rprofile.site (Windows) or .Rprofile (Linux/MacOS" ) 
+}
 
 
-	# name of the start-up file (this file) .. used for initializing parallel processing
-	ecomod.rprofile  = file.path( ecomod.directory, "ecomod.rprofile.r" )  
+# ecomod.workdirectory
+if (is.null( ecomod.workdirectory )) {
+	ecomod.workdirectory = getwd()
+} else {
+	setwd( ecomod.workdirectory )
+}
+
+
+# name of the start-up file (this file) .. used for initializing parallel processing
+ecomod.rprofile  = file.path( ecomod.directory, "ecomod.rprofile.r" )  
 	
 
-	# load bootstrapping functions to finish initial setup
-	startupdirectory = file.path( ecomod.directory, "common", "src", "_Rfunctions", "startup" )
-	startupfiles = list.files( path=startupdirectory, full.names=T, recursive=T,  ignore.case=T, include.dirs=F  )
-	for ( sf in startupfiles ) source( sf )
+# load bootstrapping functions to finish initial setup
+startupdirectory = file.path( ecomod.directory, "common", "src", "_Rfunctions", "startup" )
+startupfiles = list.files( path=startupdirectory, full.names=T, recursive=T,  ignore.case=T, include.dirs=F  )
+for ( sf in startupfiles ) source( sf )
 
 
 
-	# base environment .. used to manipulate namespace 
-  lib.init = search()
-  obj.init = ls()
-  namespaces.init = loadedNamespaces()
+# base environment .. used to manipulate namespace 
+lib.init = search()
+obj.init = ls()
+namespaces.init = loadedNamespaces()
 
 
