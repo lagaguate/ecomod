027762922e49eadec17f87fab3d875b7ec9e71a3
Jae S Choi <choijaeseok@gmail.com>
2013-09-30 15:47:32 -0300

    updates to SSA interface to snowcrab data

--- model.ssa/src/ssa.spatial.snowcrab.r
+++ model.ssa/src/ssa.spatial.snowcrab.r
@@ -7,8 +7,9 @@
 # Basic logistic with spatial processes  
 # Using: logistic model as base
 # dX/dt = rX(1-X/K)
+# Solution via SSA -- Gillespie Alogrithm:  direct computation of everything
 
-  
+ 
 
   # set.seed(1)
 
@@ -30,6 +31,9 @@
   p$spatial.domain = "snowcrab"  # spatial extent and data structure 
   p = model.pde.define.spatial.domain(p)
   
+  # rows are easting (x);  columns are northing (y) --- in R 
+  # ... each cell has dimensions of 1 X 1 km ^2
+
   # rate of increase per day .. for snow crab r~1 per capita per year ===> therefore per day = 1/365
   # p$modeltimes = c( 0, 5, 100, 200, 400, 800, 1600 )  # times at which output is desired
   p$modeltimeoutput = c( 0, 5, 10, 20, 21, 22, 23, 24, 25, 40, 50 )  # times at which output is desired
@@ -49,14 +53,17 @@
   
  
   # model parameters
-  p$b = 3 / 365 # birth rate
-  p$d = 2 / 365 # death rate
-  p$r = 1   ### not really used in SSA but must match above
+  p$b = matrix( nrow=p$nr, ncol=p$nc, data=3/365 ) # birth rate
+  p$d = matrix( nrow=p$nr, ncol=p$nc, data=2/365 ) # death rate
+  
+  p$r = p$b - p$d   ### not used in SSA but must match above
 
-
+  # p$K = matrix( nrow=p$nr, ncol=p$nc, data=rnorm( p$nrc, mean=p$K, sd=p$K/10) )
   p$K = model.pde.external.db( p=p, method="snowcrab.male.mature", variable="abundance.mean" ) 
   p$K[ p$inothabitat ] = p$eps
-  iifin = which (!is.finite( K) ) 
+
+
+  iifin = which (!is.finite( p$K) ) 
   if (length(iifin)>0) p$K[iifin] = p$eps
 
 
@@ -73,10 +80,10 @@
   
   # diffusion coef d=D/h^2 ; h = 1 km; per year (range from 1.8 to 43  ) ... using 10 here 
   # ... see b ulk estimation in model.lattice/src/_Rfunctions/estimate.bulk.diffusion.coefficient.r
-  p$dr=10 
-  p$dc=10 
+  p$dr=matrix( ncol=p$nc, nrow=p$nr, data=10)
+  p$dc=matrix( ncol=p$nc, nrow=p$nr, data=10) 
   p$Da = matrix( ncol=p$nc, nrow=p$nr, data=10 ) 
-
+  # p$Da = matrix( ncol=p$nc, nrow=p$nr, data=rnorm( p$nrc, mean=p$Da, sd=p$Da/10 ) )
   
   
   
@@ -86,94 +93,95 @@
   p$t.censusinterval = p$t.end / p$n.times
   p$modeltimeoutput = seq( 0, p$t.end, length=p$n.times )  # times at which output is desired .. used by pde
  
-  
-  # rows are easting (x);  columns are northing (y) --- in R 
-  # ... each cell has dimensions of 1 X 1 km ^2
- 
-  expand.parameters.in.space = FALSE
-  if (expand.parameters.in.space) {
-    p$r = matrix( nrow=p$nr, ncol=p$nc, data=rnorm( p$nn, mean=p$r, sd=p$r/10) )
-    p$K = matrix( nrow=p$nr, ncol=p$nc, data=rnorm( p$nn, mean=p$K, sd=p$K/10) )
-    p$Da = matrix( ncol=p$nc, nrow=p$nr, data=rnorm( p$nn, mean=p$Da, sd=p$Da/10 ) ) 
-  }
-
-
 
   out = array( 0, dim=c( p$nr, p$nc, p$n.times ) )
   
 
 
 
-
-
-
-
-  ####################   SSA 
-  # Spatial prototype for Gillespie Alogrithm:  direct computation of everything
-
-
-  simtime = itime = next.output.time = nevaluations = 0
+  nevaluations = 0
+  simtime = 0
+  itime = 0
+  next.output.time = 0
+  next.day = 0
  
-  # initiate P the propensities 
-  P = array( 0, dim=c( nr, nc, np ) )
-  nP = length(P)
-  jr = 1:nr
-  jc = 1:nc
-  for ( ip in 1:np ) P[,,ip] = eval( parse( text=RE.logistic.spatial( ip ) ) ) 
-  P.total = sum(P)
- 
+  attach(p) 
   
-# Rprof()
-
-  repeat {
+    # initiate P the propensities 
     
-    prop = .Internal(pmax(na.rm=FALSE, 0, P/P.total  ))   # using .Internal is not good syntax but this gives a major perfance boost > 40%
-    j = .Internal(sample( nP, size=1, replace=FALSE, prob=prop ) )
+    P = array( 0, dim=c( nr, nc, np ) )
+    nP = length(P)
+    jr = 1:nr
+    jc = 1:nc
+    for ( ip in 1:np ) P[,,ip] = eval( parse( text=RE.logistic.spatial.full( ip ) ) ) 
+    P.total = sum(P)
+ 
 
-    # remap random element to correct location and process
-    jn  = floor( (j-1)/nn ) + 1  # which reaction process
-    jj = j - (jn-1)*nn  # which cell 
+    # Rprof()
 
-    # focal cell coords
-    cc = floor( (jj-1)/nr ) + 1
-    cr = jj - (cc-1) * nr 
+    repeat {
+ 
+      prop = .Internal(pmax(na.rm=FALSE, 0, P/P.total  ))   # using .Internal is not good syntax but this gives a major perfance boost > 40%
+      j = .Internal(sample( nP, size=1, replace=FALSE, prob=prop ) )
 
-    # determine the appropriate operations for the reaction
-    o = NU.logistic.spatial(jn) 
+      # remap random element to correct location and process
+      jn  = floor( (j-1)/nrc ) + 1  # which reaction process
+      jj = j - (jn-1)*nrc  # which cell 
 
-    no = dim(o)[1]
-    ro = .Internal( pmin( na.rm=FALSE, nr, .Internal( pmax( na.rm=FALSE, no, cr + o[u,1] ) ) ) )
-    co = .Internal( pmin( na.rm=FALSE, nc, .Internal( pmax( na.rm=FALSE, no, cc + o[u,2] ) ) ) )
+      # focal cell coords
+      cc = floor( (jj-1)/nr ) + 1
+      cr = jj - (cc-1) * nr 
 
-    for( u in 1:no ) {
-      # update state vector (X) 
-      X[ro[u],co[u]] = .Internal( pmax( na.rm=FALSE, 0, X[ro[u],co[u]] + o[u,3] ) )
+      # determine the appropriate operations for the reaction
+      o = NU.logistic.spatial(jn) 
 
-      # update propensity in focal and neigbouring cells 
-      jr = .Internal( pmin( na.rm=FALSE, nr, .Internal( pmax( na.rm=FALSE, 1, ro[u] + c(-1,0,1) ) ) ) )
-      jc = .Internal( pmin( na.rm=FALSE, nc, .Internal( pmax( na.rm=FALSE, 1, co[u] + c(-1,0,1) ) ) ) )
-
-      for ( iip in 1:np) {
-        dP = eval( parse(text=RE.logistic.spatial( iip ))) 
-        P.total = P.total + sum( P[jr,jc,iip] - dP )
-        P[jr,jc,iip] = dP
+      no = dim(o)[1]
+      ro = .Internal( pmin( na.rm=FALSE, nr, .Internal( pmax( na.rm=FALSE, no, cr + o[,1] ) ) ) )
+      co = .Internal( pmin( na.rm=FALSE, nc, .Internal( pmax( na.rm=FALSE, no, cc + o[,2] ) ) ) )
+  
+      # update state (X) 
+      coord = cbind(ro,co)
+      X[coord] = .Internal( pmax( na.rm=FALSE, 0, X[coord] + o[,3] ) )
+     
+      # update propensity (P) in focal and neigbouring cells 
+      for( u in 1:no ) {
+        jr = .Internal( pmin( na.rm=FALSE, nr, .Internal( pmax( na.rm=FALSE, 1, ro[u] + c(-1,0,1) ) ) ) )
+        jc = .Internal( pmin( na.rm=FALSE, nc, .Internal( pmax( na.rm=FALSE, 1, co[u] + c(-1,0,1) ) ) ) )
+        for ( iip in 1:np) {
+          dP = eval( parse(text=RE.logistic.spatial.full( iip ))) 
+          P.total = P.total + sum( P[jr,jc,iip] - dP )
+          P[jr,jc,iip] = dP
+        }
       }
+
+      # nevaluations = nevaluations + 1
+      
+      time.increment = -(1/P.total)*log(runif( 1))
+      simtime = simtime + time.increment  # ... again to optimize for speed
+      
+      if (simtime > next.daily.process) {
+        next.daily.process = next.daily.process + 1
+        do.fishing.activity( simetime )
+      }
+
+
+      if (simtime > next.output.time ) {
+        next.output.time = next.output.time + t.censusinterval 
+        itime = itime + 1  # time as index
+        out[,,itime] = X[]
+        P.total = sum(P) # reset P.total to prevent divergence due to floating point errors
+        cat( paste( itime, round(P.total), round(sum(X)), Sys.time(), sep="\t\t" ), "\n" )
+        # cat( paste( itime, round(P.total), round(sum(X)), nevaluations, Sys.time(), sep="\t\t" ), "\n" )
+        # nevaluations = 0 # reset
+        image( out[,,itime], col=heat.colors(100)  )
+      }
+    
+      if (simtime > t.end ) break()
+    
     }
 
-    nevaluations = nevaluations + 1
-    simtime = simtime - (1/P.total) * log( runif( 1))   # ... again to optimize for speed
-    if (simtime > t.end ) break()
-    if (simtime > next.output.time ) {
-      next.output.time = next.output.time + t.censusinterval 
-      itime = itime + 1  # time as index
-      out[,,itime] = X[]
-      P.total = sum(P) # reset P.total to prevent divergence due to floating point errors
-      cat( paste( itime, round(P.total), round(sum(X)), nevaluations, Sys.time(), sep="\t\t" ), "\n" )
-      nevaluations = 0 # reset
-      image( out[,,itime], col=heat.colors(100)  )
-    }
-  }
 
+  detach(p) 
 
 #  Rprof(NULL)
 
@@ -182,6 +190,10 @@
 
   plot( seq(0, t.end, length.out=n.times), out[1,1,], pch=".", col="blue", type="b" ) 
   
+
+
+
+
 
 
 
@@ -203,7 +215,7 @@
     cwind = floor(p$nc/10*4.5):floor(p$nc/10*5.5)
     A = array( 0, dim=c(p$nr, p$nc ) ) 
     A[ rwind, cwind ] = round( p$K * 0.8 )
-    # X[,] = round( runif(nn) * K )
+    # X[,] = round( runif(p$nrc) * K )
   }
  
   
