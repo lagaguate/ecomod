65fd39f5c42ec881df7a5a0a3df899019810d849
Jae S Choi <choijaeseok@gmail.com>
2014-05-30 11:41:26 -0300

    habitat db rename file

---
+++ habitat/src/habitat.r
@@ -0,0 +1,29 @@
+   
+  loadfunctions ( "habitat", functionname="habitat.parameters.r" ) 
+  
+  ### ENSURE all following incoming data are up to date before running this:
+  ### i.e. : 
+  ### loadfunctions( "bathymetry", functionname="bathymetry.r" ) 
+  ### loadfunctions( "substrate", functionname="substrate.r" ) 
+  ### loadfunctions( "groundfish", functionname="1.groundfish.r" ) 
+  ### loadfunctions( "taxonomy", functionname="taxonomy.r" ) 
+  ### loadfunctions( "temperature", functionname="temperature.r" ) 
+  
+  loadfunctions ( "habitat", functionname="habitat.temperatures.r" ) 
+  
+
+
+  ### ENSURE all following incoming habitat data are complete before running this:
+  ### i.e. :
+  ### loadfunctions ( "bio", functionname="bio.r" )  
+  ### loadfunctions ( "speciesarea", functionname="speciesarea.r" ) 
+  ### loadfunctions ( "speciescomposition", functionname="speciescomposition.r" ) 
+  ### loadfunctions ( "sizespectrum", functionname="sizespectrum.r" ) 
+  ### loadfunctions ( "metabolism", functionname="metabolism.r" ) 
+  ### loadfunctions ( "condition", functionname="condition.r" ) 
+
+  loadfunctions ( "habitat", functionname="habitat.complete.r" ) 
+ 
+
+ 
+
