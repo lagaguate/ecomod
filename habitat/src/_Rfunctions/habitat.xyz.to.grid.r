fdd33b3f1c6b40d7ba13a13d039ace6677545447
Jae S Choi <choijaeseok@gmail.com>
2014-05-30 07:55:34 -0300

    habitat db rename file

--- habitat/src/_Rfunctions/habitat.xyz.to.grid.r
+++ habitat/src/_Rfunctions/habitat.xyz.to.grid.r
@@ -1,17 +1,23 @@
 
   habitat.xyz.to.grid = function( p, redo=FALSE ) {
 
-    fn = file.path( project.directory( "habitat" ), "data",  p$spatial.domain, paste( "xyz2grid", "rdata", sep="." ) ) 
+    ddir = project.directory( "habitat" , "data" )
+
+    fn = file.path( ddir, paste( "habitat.xyz2grid", p$spatial.domain, "rdata", sep="." ) ) 
  
     if (redo) {
+    
       loadfunctions("bathymetry")  # base structure from baythmetry db
       H = bathymetry.db( p=p, DS="baseline" )  
       row = round(( H$plon - min(H$plon) ) / p$pres ) + 1
       col = round(( H$plat - min(H$plat) ) / p$pres ) + 1
       row.col = cbind(row, col)
       save ( row.col, file=fn, compress=TRUE )
+
     } else {
+
       load(fn) 
+
     }
       
     return(row.col) 
