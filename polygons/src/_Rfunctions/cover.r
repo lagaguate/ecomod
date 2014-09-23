ead33d0a60fbbd9b60f81742ae8841c3627caaa8
PopulationEcologyDivision <mike.mcmahon@dfo-mpo.gc.ca>
2014-09-23 13:28:28 -0300

    Re-organized the polygon data into folders
    First attempt at this re-organization

---
+++ polygons/src/_Rfunctions/cover.r
@@ -0,0 +1,21 @@
+#MMM Sept 2014 - altered paths to reflect new ecomod polygon paths
+cover= function(x,area="all", wd=wd <- file.path(project.directory('polygons'),'data')){
+  require(PBSmapping)
+
+  borders= read.csv(file=file.path(wd,"Management_Areas","Fisheries","areaborders.csv"), head=T, sep=",")
+  b=borders[which(borders$area==area),]
+
+# read in shapefiles
+#--------------------------------------
+  land= importShapefile(file.path(wd,"Basemaps","Terrestrial","landmass_region"))
+  coast=importShapefile(file.path(wd,"Basemaps","Marine","Coastline","coastline_polyline"))
+
+#Overlay land and coastline such that any bad data (on land) is hidden
+  addPolys(land, col="khaki", border="khaki")
+  addLines(coast, col="black")
+  abline(h=b$slat, lwd=3)
+  abline(h=b$nlat, lwd=3)
+  abline(v=-b$wlon, lwd=3)
+  abline(v=-b$elon, lwd=3)
+
+}
