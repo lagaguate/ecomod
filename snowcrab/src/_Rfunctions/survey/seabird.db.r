75b4d44722a6726e5116412ee8c2e2d43380150c
Jae S Choi <choijaeseok@gmail.com>
2015-01-09 09:28:31 -0400

    net mensuration seabird tweaks

--- snowcrab/src/_Rfunctions/survey/seabird.db.r
+++ snowcrab/src/_Rfunctions/survey/seabird.db.r
@@ -153,14 +153,19 @@
           
           M$timestamp = as.POSIXct( M$chron, tz="ADT" )
           res = bottom.contact( id=id, x=M , settimestamp=rid$setChron[i], setdepth=rid$setZx[i], tdif.min=3, tdif.max=15, eps.depth=2 )
-          
+         # res = bottom.contact( id=id, x=M , settimestamp=rid$setChron[i], setdepth=rid$setZx[i], tdif.min=3, tdif.max=15, eps.depth=2, plot.data=TRUE )
+           
 ## --- NOTE ... modal(2) and smooth (1)  seem to work best ... focus upon these methods with seabird data
-          
+          res$res$t0 = res$smooth.method[1]
+          res$res$t0 = res$smooth.method[2]
+          res$res$dt = res$smooth.method[2] -  res$smooth.method[1]
+
           sbStats = rbind( sbStats, cbind( seabird_uid=id, res$res ) )
         }
 
 
         sbStats$seabird_uid =  as.character(sbStats$seabird_uid)
+
         sbdt = sbStats$dt
         sbStats$dt = NA
         i = which(!is.na( sbdt ) )
