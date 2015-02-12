6992e47c32630f1b529e22a5cd6c2082e524add4
Adam Cook <amcook127@gmail.com>
2015-02-12 11:00:41 -0400

    getting habitat.model.db running and figures for assessment

--- snowcrab/src/_Rfunctions/figures.tables/figure.timeseries.gam.potential.habitat.r
+++ snowcrab/src/_Rfunctions/figures.tables/figure.timeseries.gam.potential.habitat.r
@@ -1,5 +1,5 @@
 
-  figure.timeseries.gam.potential.habitat= function( outdir=file.path(project.directory('snowcrab'), "assessments","2013"), all.areas=T ) {
+  figure.timeseries.gam.potential.habitat= function( outdir=file.path(project.directory('snowcrab'), "assessments","2014"), all.areas=T ) {
  
     set = snowcrab.db( DS="set.merge.det")
   
@@ -41,31 +41,33 @@ fdir <-file.path( project.directory("snowcrab"), "R", "gam","habitat" )
       ylim=range(c(td$sa.region), na.rm=T); ylim[1]=ylim[1]-0.1*ylim[2]; ylim[2] = ylim[2]+ylim[2]*0.2
       xlim=range(td$year); xlim[1]=xlim[1]-0.5; xlim[2]=xlim[2]+0.5
 
+   
       dir.create( outdir, recursive=T, showWarnings=F )
-      fn = file.path( outdir, paste( 'potential','habitat', "combined", "png", sep="." ) )
-      Cairo( file=fn, type="png", bg="white",  units="in", width=5, height=6, dpi=300)
-      setup.lattice.options()
-      pl = xyplot( sa.region~year|region, data=td, 
-        layout=c(1,n.areas), 
-        par.strip.text = list(cex=3.0),
-        par.settings=list(  
-          axis.text=list(cex=2.5), 
-          par.main.text = list(cex=4),
-          layout.heights=list(strip=0.2, panel=1, main=0.5 ) 
-        ),
-        xlim=xlim, scales = list(y = "free"),
-       xlab=list("Year", cex=3), ylab=list("Potential Snow Crab Habitat '000 km^2", cex=3),
-        panel = function(x, y,  ...) {
-          mx <- median(y)
-                 panel.xyplot(x, y, type="b", lty=1, lwd=6, pch=20, col=c(rep("black",(length(y)-1)),'red'), ...)
-          panel.abline(h=mx, col="gray75", lwd=5, ...)
-        }
+      fn = file.path( outdir, paste( 'potential.habitat.combined', sep="." ) )
+  
+        cex.main = 1.4
+    cex.lab = 1.3
+    cex.axis = 1.3
+    Cairo( file=fn, type="pdf", bg="white",  units="in", width=6, height=8 )
+    setup.lattice.options()
+    pl = xyplot( sa.region~year|region, data=td, 
+          layout=c(1,n.regions), xlim=xlim, 
+               xlab="Year", ylab=list("Potential Snow Crab Habitat '000 km^2"),
+              cex.lab=cex.lab, cex.axis=cex.axis, cex.main = cex.main,scales = list(y = "free"),
+              panel = function(x, y, ...) {
+             panel.xyplot(x, y, type="b", lty=1, lwd=2, pch=20, col=c(rep("black",(length(y)-1)),'red'), ...)
+             panel.abline(h=median(y), col="gray75", ...)
+         }
       )
+   
        
       print(pl)
       dev.off()
-      cmd( "convert -trim -frame 10x10 -mattecolor white ", fn, fn )
-    
+    cmd( "convert   -trim -quality 9  -geometry 200% -frame 2% -mattecolor white -antialias ", paste(fn, "pdf", sep="."),  paste(fn, "png", sep=".") )
+   
+
+
+
     }
 
 
