427483d5ebd2e629d8ac87729e2f6cb2f565f65d
Jae S Choi <choijaeseok@gmail.com>
2015-02-21 14:10:09 -0400

    netmensuration: marport annual steps

--- groundfish/src/_Rfunctions/net_mensuration/marport.db.r
+++ groundfish/src/_Rfunctions/net_mensuration/marport.db.r
@@ -1,6 +1,6 @@
 
 
-marport.db = function( DS, p ){
+marport.db = function( DS, p, YRS=NULL ){
     
   if (!file.exists( p$marport.dir )) {
     mk.Dir = readline("Directory not found, shall we create it? Yes/No (Default No) : ")
@@ -14,45 +14,64 @@ marport.db = function( DS, p ){
 
  
   if(DS %in% c("marport", "marport.redo"))  {
+    
+    if (is.null (YRS) ) YRS = p$netmensuration.years 
+    
     basedata=NULL
-    fn=file.path( p$marport.dir, paste( "marport", "rdata", sep="." ))
-    if(DS == "marport"){
-      if (file.exists(fn)) load(fn)
-      return(basedata)
+    dir.create( file.path( p$marport.dir, "basedata"), recursive=TRUE, showWarnings=FALSE ) 
+      
+    if(DS == "basedata"){
+      out = NULL
+      for ( YR in YRS ) {
+        basedata=NULL
+        fn = file.path( p$marport,  "basedata",  paste( "marport", "basedata", YR, "rdata", sep="." ))
+        if ( file.exists(fn)) {
+          load(fn)
+          out = rbind( out, basedata )
+        }
+      }
+      return(out)
     }
-    
-    # configs
-    # p$marport.dir = file.path("C:", "Users", "MundenJ", "Desktop", "Marport")
-    filelist4 = list.files(path=p$marport.dir, pattern="^config.*.ini$", full.names=T, recursive=TRUE, ignore.case=TRUE)
-    cfg = data.frame( configroot = dirname( filelist4 ), fn=filelist4, stringsAsFactors=FALSE )
-    
-    filelist1 = list.files(path=p$marport.dir, pattern=".log$", full.names=T, recursive=TRUE, ignore.case=TRUE)
-    filelist2 = list.files(path=p$marport.dir, pattern=".gps$", full.names=T, recursive=TRUE, ignore.case=TRUE)
-    filelist3 = list.files(path=p$marport.dir, pattern=".sgp$", full.names=T, recursive=TRUE, ignore.case=TRUE)
-   
-    filelist1 = gsub( ".log$", "", filelist1)
-    filelist2 = gsub( ".gps$", "", filelist2)
-    filelist3 = gsub( ".sgp$", "", filelist3)
-    
-    fileroots = unique( c( filelist1, filelist2, filelist3 ) )
-    filelist = data.frame( fileroots=fileroots, configroot = dirname( fileroots ), stringsAsFactors=FALSE )
-    filelist = merge( filelist, cfg, by="configroot", all.x=TRUE, all.y=FALSE )
-    
-    no.files = nrow(filelist)
-    for ( ii in 1:no.files ) {
-      fl = filelist$fileroots[ii]
-      sensorconfig = filelist$fn[ii]
-      print(fl)
-      j = load.marport.rawdata( fl, sensorconfig )  # variable naming conventions in the past
-      if (is.null(j)) next()
-      j$rootname=fl
-      basedata = rbind( basedata, j)
-    }
-    
-    # Include mission as a variable (also trip and year)
-    g=substring(basedata$rootname,54,63)      
-    basedata$mission = paste(g, basedata$set, sep=".")
-    basedata$year = substring(basedata$mission, 4,7)
+
+       
+    for ( YR in YRS ) {
+      basedata = NULL
+      fn = file.path( p$marport,  "basedata",  paste( "marport", "basedata", YR, "rdata", sep="." ))
+  
+      rawdata.dir = file.path( p$marport, "marport.logs", YR )
+      
+      # configs
+      # p$marport.dir = file.path("C:", "Users", "MundenJ", "Desktop", "Marport")
+      filelist4 = list.files(path=rawdata.dir, pattern="^config.*.ini$", full.names=T, recursive=TRUE, ignore.case=TRUE)
+      cfg = data.frame( configroot = dirname( filelist4 ), fn=filelist4, stringsAsFactors=FALSE )
+      
+      filelist1 = list.files(path=rawdata.dir, pattern=".log$", full.names=T, recursive=TRUE, ignore.case=TRUE)
+      filelist2 = list.files(path=rawdata.dir, pattern=".gps$", full.names=T, recursive=TRUE, ignore.case=TRUE)
+      filelist3 = list.files(path=rawdata.dir, pattern=".sgp$", full.names=T, recursive=TRUE, ignore.case=TRUE)
+     
+      filelist1 = gsub( ".log$", "", filelist1)
+      filelist2 = gsub( ".gps$", "", filelist2)
+      filelist3 = gsub( ".sgp$", "", filelist3)
+      
+      fileroots = unique( c( filelist1, filelist2, filelist3 ) )
+      filelist = data.frame( fileroots=fileroots, configroot = dirname( fileroots ), stringsAsFactors=FALSE )
+      filelist = merge( filelist, cfg, by="configroot", all.x=TRUE, all.y=FALSE )
+      
+      no.files = nrow(filelist)
+      for ( ii in 1:no.files ) {
+        fl = filelist$fileroots[ii]
+        sensorconfig = filelist$fn[ii]
+        print(fl)
+        j = load.marport.rawdata( fl, sensorconfig )  # variable naming conventions in the past
+        if (is.null(j)) next()
+        j$rootname=fl
+        basedata = rbind( basedata, j)
+      }
+      
+      # Include mission as a variable (also trip and year)
+      g=substring(basedata$rootname,54,63)      
+      basedata$mission = paste(g, basedata$set, sep=".")
+      basedata$year = substring(basedata$mission, 4,7)
 
     print ( "Check this section with Jenna" ) 
     {
@@ -79,16 +98,19 @@ marport.db = function( DS, p ){
     }
 
 
-    # rename mission to id, so comparisons with Scanmar are easier
-    basedata$id = basedata$mission
-    
-    # Make year numeric and as trip as a variable
-    basedata$year=as.numeric(basedata$year)
-    basedata$trip = substring(basedata$mission, 8,10)
-    basedata$trip=as.numeric(basedata$trip)
-    
-    save(basedata, file=fn, compress= TRUE)
-    return (fn )
+      # rename mission to id, so comparisons with Scanmar are easier
+      basedata$id = basedata$mission
+      
+      # Make year numeric and as trip as a variable
+      basedata$year=as.numeric(basedata$year)
+      basedata$trip = substring(basedata$mission, 8,10)
+      basedata$trip=as.numeric(basedata$trip)
+      
+      save(basedata, file=fn, compress= TRUE)
+      print( fn)
+    }
+
+    return (YRS )
   }
 
 
