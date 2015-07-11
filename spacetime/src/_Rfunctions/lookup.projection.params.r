aad1645977eb969de81c3052951e23856876e187
Jae S Choi <choijaeseok@gmail.com>
2015-07-11 14:30:37 -0300

    clean up lookup table

--- spacetime/src/_Rfunctions/lookup.projection.params.r
+++ spacetime/src/_Rfunctions/lookup.projection.params.r
@@ -1,12 +1,14 @@
   lookup.projection.params = function(x) {
+    # for examples, see: 
+    # http://www.progonos.com/furuti/MapProj/Normal/TOC/cartTOC.html
     out  = switch( x,
-      utm20 = "+proj=utm +ellps=WGS84 +zone=20 ", 
-      utm20.substrate = "+proj=utm +datum=NAD83 +zone=20 ",
-      lambert.conic = "+proj=lcc +ellps=WGS84 +lon_0=63W +lat_0=45N +lat_1=43N +lat_2=47N ",
-      lambert.conic.4t = "+proj=lcc +ellps=WGS84 +lon_0=63W +lat_0=47N +lat_1=45N +lat_2=49N ",
-      lambert.conic.canada.east = "+proj=lcc +ellps=WGS84 +lon_0=62W +lat_0=45N ",
-      tmercator     = "+proj=tmerc +ellps=WGS84 +x_0=500000 +y=-400000 +lon_0=90w "
+      utm20             = "+proj=utm +ellps=WGS84 +zone=20 ", 
+      utm20.substrate   = "+proj=utm +datum=NAD83 +zone=20 ",
+      lambert.conic.equidist    = "+proj=eqdc +ellps=WGS84 +lon_0=63W +lat_0=45N +lat_1=43N +lat_2=47N ",
+      lambert.conic             = "+proj=lcc +ellps=WGS84  +lon_0=63W +lat_0=45N +lat_1=43N +lat_2=47N ",
+      lambert.conic.4t          = "+proj=lcc +ellps=WGS84  +lon_0=63W +lat_0=47N +lat_1=45N +lat_2=49N ",
+      lambert.conic.canada.east = "+proj=lcc +ellps=WGS84  +lon_0=62W +lat_0=45N +lat_1=43N +lat_2=47N  ",
+      tmercator         = "+proj=tmerc +ellps=WGS84 +x_0=500000 +y=-400000 +lon_0=90w "
     )
     return ( out )
   }
-
