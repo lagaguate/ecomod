fd6fcfbef09ef941b3f944ad7020131ddf245573
Jae S Choi <choijaeseok@gmail.com>
2015-09-15 12:27:31 -0300

    added ecomod keyword search functionality

--- _ecomodSetup/ecomod.help.r
+++ _ecomodSetup/ecomod.help.r
@@ -1,12 +1,9 @@
 
-ecomod.help = function( h=NULL, allUsage=FALSE, filepattern="\\.r$" ) {
-  #// ecomod.help( "search item" ) - searches for help inside a function
-  
-  if (is.null(h)) ecomod.help( "ecomod.help" )
-  
+ecomod.help = function( h="ecomod.help", filepattern="\\.r$") {
+  #// ecomod.help( "ecomod function name" ) - searches for help inside a function
+   
   fn.docs = file.path( ecomod.workdirectory, "ecomod.help.docs.rdata")
   fn.code = file.path( ecomod.workdirectory, "ecomod.help.sourcecode.rdata" )
-  
   
   if (h=="refresh") {
     #// ecomod.help( "refresh" ) - refresh list of ecomod functions available locally
@@ -30,43 +27,29 @@ ecomod.help = function( h=NULL, allUsage=FALSE, filepattern="\\.r$" ) {
     }
     save( docs, file=fn.docs, compress=TRUE )
     save( code, file=fn.code, compress=TRUE )
+    print( paste( "Local help files saved to ", ecomod.workdirectory ))
   }
 
-  
   if ( !file.exists( fn.docs) | !file.exists( fn.code) ) ecomod.help( "refresh" )
-  
   load( fn.docs )
-  load( fn.code )
 
-  mm = grep( h, names( docs), ignore.case=TRUE )
-  if (length(mm) > 0 ) {
+  docs.names = names( docs)
+
+  mm = grep( h, docs.names, ignore.case=TRUE )
+  if (length(mm) == 0) return( "Function not found. Try refreshing local help with: ecomod.help('refresh')) ")
+
     cat("\n")
     cat( "Function is found at:" )
     cat("\n\n")
-    cat( names( docs)[mm] )
+    cat( paste( " ", docs.names[mm], collapse="\n") )
     cat( "\n\n" )
     cat( "Help documentation:")
     cat( "\n\n  " )
-    cat( paste( docs[[mm]], collapse="\n  "  ), "\n" ) 
-  }
 
-  if (length(mm)==0 | allUsage ) {
-    extractData = function(X, h, ... ) { 
-      ee = grep( h, X, ignore.case=TRUE, ... )
-      out = NULL
-      if (length(ee) >0 ) out = X[ee]
-      return(out)
-    }
-    res = lapply( code, extractData, h=h )
-    rr = which(sapply( res, length ) > 0)
-
-    output = "No matches" 
-    if (length( rr) > 0 )  output = res[rr]
-    
-    cat( "\nFiles and [line numbers] where matches were found:\n" )
-    print(output)
-
-  }
+    out = paste( docs[[mm]],  collapse="\n  "  )
+    cat( ( out) )
+    cat( "\n" )
+    cat( "\n" )
 }
 
 
