1e0c6e30c942e9b447e755c4842378c65b22c30d
Jae S Choi <choijaeseok@gmail.com>
2015-11-25 15:22:29 -0400

    add state space model template for generic timeseries

--- bathymetry/src/bathymetry.spde.r
+++ bathymetry/src/bathymetry.spde.r
@@ -19,13 +19,13 @@
 
   p = spacetime.parameters(p)  # load spde defaults
 
-  p$dist.max = 75 # length scale (km) of local analysis .. for acceptance into the local analysis/model
+  p$dist.max = 100 # length scale (km) of local analysis .. for acceptance into the local analysis/model
   p$dist.mwin = 5 # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
   p$dist.pred = 0.95 # % of dist.max where **predictions** are retained (to remove edge effects)
  
   ## this changes with resolution: at p$pres=0.25 and a p$dist.max=25: the max count expected is 40000
   p$n.min = 30
-  p$n.max = 6000 # numerical time/memory constraint
+  p$n.max = 7500 # numerical time/memory constraint
 
   p$expected.range = 50 # km , with dependent var on log scale
   p$expected.sigma = 1e-1  # spatial standard deviation (partial sill) .. on log scale
@@ -75,7 +75,7 @@
   # RAM reqiurements are a function of data density and mesh density .. currently ~ 12 GB / run
   # p$clusters = "localhost"  # if serial run, send a single cluster host
   # p$clusters = c( "hyperion",  "nyx", "tartarus", "kaos", "tethys" ) 
-  p$clusters = rep( "localhost", 8 )
+  p$clusters = rep( "localhost", 6 )
   # p$clusters = c( rep( "hyperion", 6 ), rep( "nyx", 24 ), rep ("tartarus", 24), rep("kaos", 24 ), rep("tethys", 6 ) )
   nS = spacetime.db( p, DS="statistics.bigmemory.size" )
     
@@ -117,55 +117,75 @@
       landmask( lonlat=V, db="worldHires",regions=c("Canada", "US"), ylim=c(36,53), xlim=c(-72,-45),
       tag="statistics" )
     }
-    oc = landmask( db="worldHires", regions=c("Canada", "US"), return.value="not.land", tag="statistics" )
+  
+    p = spacetime.db( p=p, DS="bigmemory.inla.filenames" )
+    
+    # predictions
+    S = attach.big.matrix(p$descriptorfile.S , path=p$tmp.datadir ) 
+ 
 
-
-  x11()
+    x11()
+    p$spatial.domain="canada.east"  # force isobaths to work in levelplot
     datarange = log( c( 5, 800 ))
     dr = seq( datarange[1], datarange[2], length.out=150)
+    oc = landmask( db="worldHires", regions=c("Canada", "US"), return.value="not.land", tag="statistics" )
     levelplot( log(S[oc,3])  ~ S[oc,1] + S[oc,2] , aspect="iso", at=dr, col.regions=color.code( "seis", dr) ,
-      contour=FALSE, labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE), cex=2, 
+      contour=FALSE, labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE), cex=2,
       panel = function(x, y, subscripts, ...) {
         panel.levelplot (x, y, subscripts, aspect="iso", rez=c(5,5), ...)
         #coastline
         cl = landmask( return.value="coast.lonlat",  ylim=c(36,53), xlim=c(-72,-45) )
         cl = lonlat2planar( data.frame( cbind(lon=cl$x, lat=cl$y)), proj.type=p$internal.crs )
-        panel.xyplot( cl$plon, cl$plat, col = "black", type="l", lwd=1 )
+        panel.xyplot( cl$plon, cl$plat, col = "black", type="l", lwd=0.8 )
+        zc = isobath.db( p=p, depths=c( 300 ) )  
+        zc = lonlat2planar( zc, proj.type=p$internal.crs) 
+        panel.xyplot( zc$plon, zc$plat, col = "gray", pch=".", cex=0.1 )
       }
     ) 
+    p$spatial.domain="canada.east.highres"
 
-
+    
+    
+    
     #predictions 
-    P = attach.big.matrix(p$descriptorfile.P , path=p$tmp.datadir )
-    pps  =  expand.grid( plons=p$plons, plats=p$plats)
-   
+
     # generate landmask
     if (index.redo) {
       V = SpatialPoints( planar2lonlat( pps, proj.type=p$internal.crs )[, c("lon", "lat" )], CRS("+proj=longlat +datum=WGS84") ) 
-      landmask( lonlat=V, db="worldHires",regions=c("Canada", "US"), ylim=c(36,53), xlim=c(-72,-45),
-      tag="predictions" )
+      landmask( lonlat=V, db="worldHires",regions=c("Canada", "US"), ylim=c(36,53), xlim=c(-72,-45), tag="predictions" )
     }
-    oc = landmask( db="worldHires",regions=c("Canada", "US"), return.value="not.land", tag="predictions" )
+    
+    p = spacetime.db( p=p, DS="bigmemory.inla.filenames" )
+  
+    P = attach.big.matrix(p$descriptorfile.P , path=p$tmp.datadir )
+    pps  =  expand.grid( plons=p$plons, plats=p$plats)
 
-
-  x11()
-    datarange = log( c( 1, 5000 ))
-    dr = seq( datarange[1], datarange[2], length.out=150)
-    levelplot( log( P[oc,2] ) ~ plons + plats, pps[oc,], aspect="iso", main="mean", at=dr, col.regions=rev(color.code( "seis", dr)) ,
+    x11()
+    p$spatial.domain="canada.east"  # force isobaths to work in levelplot
+    datarange = log( c( 5, 4000 ))
+    dr = seq( datarange[1], datarange[2], length.out=100)
+    oc = landmask( db="worldHires", regions=c("Canada", "US"), return.value="not.land", tag="predictions" )
+    levelplot( log( P[oc,2] ) ~ plons + plats, pps[oc,], aspect="iso", main=NULL, at=dr, col.regions=rev(color.code( "seis", dr)) ,
       contour=FALSE, labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE),
         panel = function(x, y, subscripts, ...) {
         panel.levelplot (x, y, subscripts, aspect="iso", rez=c(1,1), ...)
         #coastline
         cl = landmask( return.value="coast.lonlat",  ylim=c(36,53), xlim=c(-72,-45)  )
         cl = lonlat2planar( data.frame( cbind(lon=cl$x, lat=cl$y)), proj.type=p$internal.crs )
-        panel.xyplot( cl$plon, cl$plat, col = "black", type="l", lwd=1 )
+        panel.xyplot( cl$plon, cl$plat, col = "steelblue", type="l", lwd=0.8 )
+   #     zc = isobath.db( p=p, depths=c(200, 400 ) )  
+   #     zc = lonlat2planar( zc, proj.type=p$internal.crs) 
+   #     panel.xyplot( zc$plon, zc$plat, col = "steelblue", pch=".", cex=0.1 )
       }
     )
- 
+    p$spatial.domain="canada.east.highres"
+
+
+    # prediction error 
     x11()
-     datarange = ( c( 0.1, 50 ))
+     datarange = log ( c( 2, 50 ))
      dr = seq( datarange[1], datarange[2], length.out=150)
-     levelplot(  P[oc,3]  ~ plons + plats, pps[oc,], aspect="iso", main="mean", at=dr, col.regions=rev(color.code( "seis", dr)) , contour=FALSE, labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE) )
+     levelplot( log( P[oc,3])~ plons + plats, pps[oc,], aspect="iso", main="mean", at=dr, col.regions=rev(color.code( "seis", dr)) , contour=FALSE, labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE) )
 
     # redo incomplete
     p = make.list( list( jj=sample( j ) ), Y=p ) 
@@ -180,7 +200,7 @@
   spacetime.db( p=p, DS="statistics.redo" )  # this also rescales results to the full domain
   spacetime.db( p=p, DS="bigmemory.inla.cleanup" )
 
-  # compute slope and curvature and then assimilate results in bathymerty
+  # compute slope and curvature and then assimilate results in bathymetry
   bathymetry.db( p=p, DS="bathymetry.spacetime.finalize.redo" )  
 
   # as the interpolation process is so expensive, regrid based off the above run
