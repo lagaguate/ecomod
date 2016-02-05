83c39d3028ab46c81e379c43e7b19323f8b44f17
amcook <amcook127@gmail.com>
2016-02-05 12:18:49 -0400

    updates on some growth stuff and lobster collectors

--- snowcrab/src/_Rfunctions/growth/histAllMixtures.r
+++ snowcrab/src/_Rfunctions/growth/histAllMixtures.r
@@ -1,8 +1,8 @@
-histAllMixtures <- function(mix.obj,x=NA,xlims=c(0,80)) {
-		if(is.na(x)) lfHist(mix.obj$x,xlims=xlims)
+histAllMixtures <- function(mix.obj,x=NA,...) {
+		if(is.na(x)) hist(mix.obj$x,...)
 	
 	for(i in 1:length(mix.obj$lambda)) {
-			addMixtures(mix.obj,i)
+			addMixtures(mix.obj,i,...)
 	}
 
 }
