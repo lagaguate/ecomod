2d2b78e643f84d41a9b6191d0c2bec0fab0276db
PopulationEcologyDivision <mike.mcmahon@dfo-mpo.gc.ca>
2016-02-19 16:54:13 -0400

    Added a scale for color blind folks

--- utility/src/_Rfunctions/colours/color.code.r
+++ utility/src/_Rfunctions/colours/color.code.r
@@ -8,8 +8,9 @@
       red.yellow.blue =  colorRampPalette(c("blue", "green", "yellow", "orange","red"), space = "Lab"), 
       nathalie = colorRampPalette( c("#d62f27", "#ed7a53", "#fccc95", "#dee3bf" , "#91a8ba", "#4575b5" ), space="Lab"),
       blue.yellow.red =  colorRampPalette(c("blue", "green", "yellow", "orange","red"), space = "Lab"), 
-      blue.yellow.blue =  colorRampPalette(c("blue", "darkgreen", "green", "yellow", "gold", "royalblue", "blue"), space = "Lab") 
-    )
+      blue.yellow.blue =  colorRampPalette(c("blue",  "green", "yellow", "gold", "royalblue", "blue"), space = "Lab"), 
+      colourblind1 =  colorRampPalette(c("#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"), space = "Lab") 
+          )
     if (length(n)==1) {
       val = cols( n )
     } else {
