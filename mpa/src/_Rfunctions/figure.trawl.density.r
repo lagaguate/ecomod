4a1fbc2988abb76db63e9763a64dc704eb51003d
Jae S Choi <choijaeseok@gmail.com>
2016-02-26 10:02:44 -0400

    tweak mpa plots

--- mpa/src/_Rfunctions/figure.trawl.density.r
+++ mpa/src/_Rfunctions/figure.trawl.density.r
@@ -1,6 +1,7 @@
 
-figure.mpa.aoi = function(p ) {
-  
+figure.trawl.density = function(p) {
+  dscols = c( snowcrab="green", groundfish="orange" ) 
+  ss = bio.db( DS="set" )
   polys = mpa.db( p=p, DS="polygons" )
   
   # redo coast to get unclipped version
@@ -10,12 +11,13 @@ figure.mpa.aoi = function(p ) {
      xlim=c(-68,-55), ylim=c(41,48),
      xaxs="i", yaxs="i", axes=TRUE )  # ie. coastline
   for (i in 1: length(polys$map.contours) ) lines( polys$map.contours[i], col=p$map.depthcontours.colours[i] )
-  lines( polys$sab.polygons["StAnnsBank_AOI"], col="slateblue4", lwd=2 ) 
+  points( ss$lon, ss$lat, pch=20, cex=0.5, col=dscols[ss$data.source]  )
+   lines( polys$sab.polygons["StAnnsBank_AOI"], col="slateblue4", lwd=2 ) 
+ 
   sab.loc = rowMeans( bbox( polys$sab.polygons["StAnnsBank_AOI"] ) )
   sp::compassRose( sab.loc[1]+2.3 , sab.loc[2]-4.3, cex= 0.7 )
   maps::map.scale( sab.loc[1]+1 , sab.loc[2]-5.1, ratio=FALSE, cex=0.8 )
-  text( sab.loc[1], sab.loc[2]+0.2, " St Anns Bank \n MPA ", pos=3, col="slateblue4", cex=0.8 )
   box()
-}
 
+}
 
