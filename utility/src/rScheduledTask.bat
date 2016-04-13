cbac6f3be4e453042a35243117c974cf420b0f86
PopulationEcologyDivision <mike.mcmahon@dfo-mpo.gc.ca>
2016-04-13 16:25:41 -0300

    Adds example of a R batch script

---
+++ utility/src/rScheduledTask.bat
@@ -0,0 +1,4 @@
+"C:\R\R-3.0.2\bin\x64\Rscript.exe" "C:\<Path to a script>\script.R"
+REM example of how a script can be run via a batch file
+REM This could then be saved as a scheduled task 
+PAUSE
