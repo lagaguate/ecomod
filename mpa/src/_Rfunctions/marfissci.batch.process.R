13c4b2621572c9388cac11139572adb8675ce12f
PopulationEcologyDivision <mike.mcmahon@dfo-mpo.gc.ca>
2016-04-21 14:47:30 -0300

    Batch now allows user choice of merging files or not

--- mpa/src/_Rfunctions/marfissci.batch.process.R
+++ mpa/src/_Rfunctions/marfissci.batch.process.R
@@ -1,5 +1,4 @@
-marfissci.batch.process <- function(folder=file.path(project.datadirectory("mpa"),"test"), 
-                                    agg.by="SPECIES_CODE"){
+marfissci.batch.process <- function(folder=file.path(project.datadirectory("mpa"),"csv","raw","batch"), combine=T){
   #' The purpose of this batch process function is to facilitate the mass
   #' generation of data products from marfis data.  It assumes that data has 
   #' been extracted via marfissci.get.data(), and the resultant csv file(s) are
@@ -8,49 +7,79 @@ marfissci.batch.process <- function(folder=file.path(project.datadirectory("mpa"
   #' When called, this batch function will allow the user to select what data 
   #' (i.e. years, species, and/or gears) should be aggregated together.  It will
   #' then generate rds files and figures for all the data.
-
-  
-  writeLines("Combining all of the csv files into a single one")
-  all.data=do.call(rbind,lapply(file.path(folder,list.files(path=folder, pattern="\\.csv$")), 
-                                read.csv, header=TRUE, sep=","))
-  
-  #get all of the unique values for the field we want to aggregate by
-  combos = unique(all.data[agg.by])
+  start.time <- Sys.time()  
  
-  library(RODBC)
-  channel <- odbcConnect("PTRAN",uid = oracle.personal.username,pwd = oracle.personal.password)
- if(agg.by == "GEAR_CODE"){
-   query = "SELECT GEAR_CODE, DESC_ENG FROM MARFISSCI.GEARS;"
- }else{
-   query = "SELECT SPECIES_CODE, DESC_ENG FROM MARFISSCI.SPECIES;"
- }
-  the.codes = sqlQuery(channel,query)
-  combos=merge(combos,the.codes)
-  
-  for (i in 3:nrow(combos)){
-    writeLines(paste0("Analysing: ", combos[i,2]))
-    #print(paste0("working on ",all.data$DESC_ENG[all.data[agg.by]==combos[i,]]))
-     this <- marfissci.process.data(all.data[which(all.data[agg.by]==combos[i,1]),],
-                                   agg.by =agg.by,
-                                   save.RDS = T,
-                                   save.CSV = F,
-                                   save.SHP = T,
-                                   agg.by.year =F,
-                                   output="RDS")
-     if (i==1) gearKeep<<-this
-     if (!is.null(this)){
-       writeLines("Generating a plot...")
-         if(agg.by == "GEAR_CODE") {
-          colour.by = "CNT_RND_WEIGHT_KGS"
+  do.it = function(all.data){
+    library(lubridate) 
+    all.data$YEAR_FISHED=year(all.data$DATE_FISHED)
+    if (range(all.data$YEAR_FISHED)[1] == range(all.data$YEAR_FISHED)[2]) {
+      years.file = range(all.data$YEAR_FISHED)[1]
+    }else{
+      years.file = paste(range(all.data$YEAR_FISHED),collapse = "_")
+    }
+    
+    agg.by=c("SPECIES_CODE","GEAR_CODE")
+    
+    
+    library(RODBC)
+    channel <- odbcConnect("PTRAN",uid = oracle.personal.username,pwd = oracle.personal.password)
+    for (a in 1:length(agg.by)){
+      #get all of the unique values for the field we want to aggregate by
+      combos = unique(all.data[agg.by[a]])
+      if(agg.by[a] == "GEAR_CODE"){
+        query = "SELECT GEAR_CODE, DESC_ENG FROM MARFISSCI.GEARS;"
+      }else{
+        query = "SELECT SPECIES_CODE, DESC_ENG FROM MARFISSCI.SPECIES;"
+      }
+      the.codes = sqlQuery(channel,query)
+      combos=merge(combos,the.codes)
+      
+      for (i in 1:nrow(combos)){
+        writeLines(paste0("Analysing: ", combos[i,2]))
+        #print(paste0("working on ",all.data$DESC_ENG[all.data[agg.by]==combos[i,]]))
+        this <- marfissci.process.data(all.data[which(all.data[agg.by[a]]==combos[i,1]),],
+                                       agg.by =agg.by[a],
+                                       save.RDS = T,
+                                       save.CSV = T,
+                                       save.SHP = T,
+                                       agg.by.year =F,
+                                       name.det=paste0(years.file,"_"),
+                                       output="RDS")
+        if (i==1) gearKeep<<-this
+        if (!is.null(this)){
+          writeLines("Generating a plot...")
+          if(agg.by[a] == "GEAR_CODE") {
+            colour.by = "CNT_RND_WEIGHT_KGS"
           }else{
             colour.by = "SUM_RND_WEIGHT_KGS"
           }
-          marfissci.simple.map(this, agg.by = agg.by, colour.by = colour.by, save.plot = T, plot.title=combos[i,2])
+          marfissci.simple.map(this, agg.by = agg.by[a], colour.by = colour.by, save.plot = T, name.det=years.file, plot.title=paste0(combos[i,2]," ",  years.file))
         }else{
-      writeLines(paste0("Insufficient data to plot a figure for ",combos[i,2]))
+          writeLines(paste0("Insufficient data to plot a figure for ",combos[i,2]))
         }
+      }
+      
     }
-  return(all.data)
+  } 
+  
+  
+  if (combine){
+  writeLines("Combining all of the csv files into a single one")
+    all.data=do.call(rbind,lapply(file.path(folder,list.files(path=folder, pattern="\\.csv$")), 
+                                  read.csv, header=TRUE, sep=","))
+    do.it(all.data)
+  } else {
+    file.names <- dir(folder, pattern ="\\.csv$")
+    for(i in 1:length(file.names)){
+      all.data <- read.csv(file.path(folder,file.names[i]),header=TRUE, sep=",")
+      do.it(all.data)
+    }
+  }
+  
+  diff=difftime(Sys.time(),start.time, units = "secs")
+  diff = format(.POSIXct(diff,tz="GMT"), "%H:%M:%S")
+  writeLines(paste0(diff, " elapsed"))
+  return(NULL)
 }
 #loadfunctions("mpa")
 
